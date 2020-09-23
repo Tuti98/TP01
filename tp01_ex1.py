a32a1a11bf568c9560085c47ef1774e013abf2d8
JeromeHumbert <54804629+JeromeHumbert@users.noreply.github.com>
2020-09-23 15:43:26 +0200

    Update tp01_ex1.py

--- tp01_ex1.py
+++ tp01_ex1.py
@@ -1,5 +1,8 @@
 """
 Programme permettant de convertir une quantité en plusieurs unités d'énergie.
+
+1J  =  0.738 ft-lb  =  0.239 cal  =  6.24*10^18 eV
+
 Unités : joule, calorie, Ft-lb et eV
   Données : Une valeur et une unité
   Indications:

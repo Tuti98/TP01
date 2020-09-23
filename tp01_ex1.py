ffa721490fbeb4306ae58b37779a970a353c6cc6
JeromeHumbert <54804629+JeromeHumbert@users.noreply.github.com>
2020-09-23 15:42:13 +0200

    Version initiale

---
+++ tp01_ex1.py
@@ -0,0 +1,17 @@
+"""
+Programme permettant de convertir une quantité en plusieurs unités d'énergie.
+Unités : joule, calorie, Ft-lb et eV
+  Données : Une valeur et une unité
+  Indications:
+        Selon l'unité entrée par l'utilisateur, afficher la conversion
+        dans les 3 autres unités.
+  Résultats : Affichage des conversions
+"""
+
+### Déclaration et initialisation des variables
+
+                   
+
+### Séquence d'opération
+
+
